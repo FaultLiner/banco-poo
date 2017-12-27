3c1f1937912d1cc6a091e277146f5ac2b1366d48
FaultLiner <santi.cervantes@hotmail.es>
2017-12-27 17:54:39 +0100

    Añadir un gestor de inversiones al cliente premium

--- src/Banco/ClientePremium.java
+++ src/Banco/ClientePremium.java
@@ -1,10 +1,10 @@
 package Banco;
 
 public class ClientePremium extends Cliente {
+    private Persona gestorDeInversiones;
 
-
-    public ClientePremium(String nombre, String dni, float saldo) {
+    public ClientePremium(String nombre, String dni, float saldo, Persona gestorDeInversiones) {
         super(nombre, dni, saldo,true);
-
+        this.gestorDeInversiones = gestorDeInversiones;
     }
 }
